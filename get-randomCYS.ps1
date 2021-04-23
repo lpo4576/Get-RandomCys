0bb95bc0a3ce5c8125a72327abc18ca6a45c837f
Larkin <55812754+lpo4576@users.noreply.github.com>
2021-04-23 13:30:57 -0500

    Uploaded initial version

---
+++ get-randomCYS.ps1
@@ -0,0 +1,12 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$names = New-Object System.Windows.Forms.OpenFileDialog
+$names.filter = "csv (*.csv)| *.csv"
+$names.Title = "Select list of names"
+[void]$names.ShowDialog()
+$names.FileName
+
+$name2 = import-csv -Path $names.FileName
+
+Get-Random -InputObject $name2
+
+pause
